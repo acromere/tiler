c44dc1d332573b23267b4d5870ab23375efa9715
msoderquist <msoderquist@mastercontrol.com>
2023-07-30 18:49:00 -0600

    Initial commit

---
+++ source/main/java/Main.java
@@ -0,0 +1,48 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class Main {
+
+	public static void main( String[] args ) {
+		Path path = Paths.get( System.getProperty( "user.home" ), "PDF" );
+
+		String project = "Eagle V";
+		char sheet = 'K';
+		int row = 1;
+		int col = 1;
+		try( Stream<Path> stream = Files.list( path ) ) {
+			// Sort by name
+			List<Path> files = stream.sorted().toList();
+
+			// Sort by last modified
+			//List<Path> files = stream.sorted( ( a, b ) -> (int)(a.toFile().lastModified() - b.toFile().lastModified()) ).toList();
+
+			for( Path source : files ) {
+				if( Files.isDirectory( source ) ) continue;
+
+				String filename = project + " - Sheet " + sheet + "-" + row + "-" + col + ".pdf";
+				Path target = path.resolve( filename );
+				if( !Objects.equals( source, target ) ) System.out.println( source + " > " + target );
+
+				Files.move( source, target );
+
+				col++;
+				if( col > 3 ) {
+					col = 1;
+					row++;
+				}
+				if( row > 3 ) {
+					row = 1;
+					sheet++;
+				}
+			}
+		} catch( Exception exception ) {
+			exception.printStackTrace();
+		}
+	}
+
+}
